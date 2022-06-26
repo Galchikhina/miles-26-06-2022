fa3f0b755e809c5143e5199a73b09d4ba7e4e930
ElenaG <lenok1586@mail.ru>
2022-06-26 23:27:35 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+            public static void main(String[] args) {
+            int ticket = 100; // цена авиабилета
+            int mile = 20; // количество рублей для одной бонусной мили
+            int mileQuant = ticket / mile; // количество бонусных миль
+            System.out.println(mileQuant);
+        }
+    }
